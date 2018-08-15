f88689f7c4e8c3efafd1aba4cfb77e5099b2f7aa
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-15 15:30:54 -0500

    Inlined BcmHost DllImports into BcmDisplay

--- VC/BcmDisplay.cs
+++ VC/BcmDisplay.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace VC
 {
@@ -12,18 +13,26 @@ namespace VC
         {
             this.display = (ushort)display;
 
-            BcmHost.Init();
-            BcmHost.GetDisplaySize(this.display, out this.width, out this.height);
+            bcm_host_init();
+            graphics_get_display_size(this.display, out this.width, out this.height);
         }
 
         public void Dispose()
         {
-            BcmHost.Deinit();
+            bcm_host_deinit();
         }
 
         public DispmanXDisplay CreateDispmanXDisplay()
         {
             return new DispmanXDisplay(this);
         }
+
+        [DllImport("bcm_host", EntryPoint = "bcm_host_init")]
+        internal extern static void bcm_host_init();
+        [DllImport("bcm_host", EntryPoint = "bcm_host_deinit")]
+        internal extern static void bcm_host_deinit();
+
+        [DllImport("bcm_host", EntryPoint = "graphics_get_display_size")]
+        internal extern static int graphics_get_display_size(ushort displayNumber, out uint width, out uint height);
     }
 }
