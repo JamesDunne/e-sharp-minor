d2dad7b57a3896784011ac25616cf904cb645dc0
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-16 08:33:18 -0500

    Simplify Program

--- Program.cs
+++ Program.cs
@@ -7,26 +7,14 @@ namespace e_sharp_minor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Startup");
-
             using (var bcmDisplay = new VC.BcmDisplay(0))
+            using (var dispmanxDisplay = bcmDisplay.CreateDispmanXDisplay())
+            using (var eglContext = dispmanxDisplay.CreateEGLContext())
             {
                 Console.WriteLine("Display[0] = {0}x{1}", bcmDisplay.width, bcmDisplay.height);
-                using (var dispmanxDisplay = bcmDisplay.CreateDispmanXDisplay())
-                {
-                    Console.WriteLine("DispmanX initialized");
-                    using (var eglContext = dispmanxDisplay.CreateEGLContext())
-                    {
-                        Console.WriteLine("EGL initialized");
-                        eglContext.SwapBuffers();
-                        Thread.Sleep(1000);
-                    }
-                    Console.WriteLine("EGL destroyed");
-                }
-                Console.WriteLine("DispmanX destroyed");
+                eglContext.SwapBuffers();
+                Thread.Sleep(1000);
             }
-
-            Console.WriteLine("Shutdown");
         }
     }
 }
