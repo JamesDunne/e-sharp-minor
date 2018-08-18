0a25f903b6e7194270164594c796418272ac0169
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-18 14:00:31 -0500

    Copied in OpenVG API bindings. Successful first window test but dimensions appear wrong.

--- Amanith/OpenVGContext.cs
+++ Amanith/OpenVGContext.cs
@@ -99,56 +99,91 @@ namespace Amanith
 
         #region VG
 
-        public int Geti(ParamType type)
-        {
-            return 0;
-        }
-
-        public float Getf(ParamType type)
-        {
-            return 0f;
-        }
-
-        public float[] Getfv(ParamType type)
-        {
-            return null;
-        }
-
-        public int[] Getiv(ParamType type)
-        {
-            return null;
-        }
-
+        [DllImport(vg, EntryPoint = "vgSeti")]
+        extern static void vgSeti(ParamType paramType, int value);
         public void Seti(ParamType paramType, int value)
         {
-
+            vgSeti(paramType, value);
         }
 
+        [DllImport(vg, EntryPoint = "vgSetf")]
+        extern static void vgSetf(ParamType paramType, float value);
         public void Setf(ParamType paramType, float value)
         {
-
+            vgSetf(paramType, value);
         }
 
+        [DllImport(vg, EntryPoint = "vgSetfv")]
+        extern static void vgSetfv(ParamType paramType, int count, float[] values);
         public void Setfv(ParamType paramType, float[] values)
         {
-
+            vgSetfv(paramType, values.Length, values);
         }
 
+        [DllImport(vg, EntryPoint = "vgGeti")]
+        extern static int vgGeti(ParamType type);
+        public int Geti(ParamType type)
+        {
+            return vgGeti(type);
+        }
+
+        [DllImport(vg, EntryPoint = "vgGetf")]
+        extern static float vgGetf(ParamType type);
+        public float Getf(ParamType type)
+        {
+            return vgGetf(type);
+        }
+
+        [DllImport(vg, EntryPoint = "vgGetVectorSize")]
+        extern static int vgGetVectorSize(ParamType type);
+        [DllImport(vg, EntryPoint = "vgGetfv")]
+        extern static void vgGetfv(ParamType type, int count, float[] values);
+        public float[] Getfv(ParamType type)
+        {
+            int size = vgGetVectorSize(type);
+            float[] vec = new float[size];
+            vgGetfv(type, size, vec);
+            return vec;
+        }
+
+        [DllImport(vg, EntryPoint = "vgGetiv")]
+        extern static void vgGetiv(ParamType type, int count, int[] values);
+        public int[] Getiv(ParamType type)
+        {
+            int size = vgGetVectorSize(type);
+            int[] vec = new int[size];
+            vgGetiv(type, size, vec);
+            return vec;
+        }
+
+        [DllImport(vg, EntryPoint = "vgSetParameteri")]
+        extern static void vgSetParameteri(uint handle, int paramType, int value);
         public void SetParameteri(uint handle, int paramType, int value)
         {
-
+            vgSetParameteri(handle, paramType, value);
         }
 
+        [DllImport(vg, EntryPoint = "vgSetParameterfv")]
+        extern static void vgSetParameterfv(uint handle, int paramType, int count, float[] values);
         public void SetParameterfv(uint handle, int paramType, float[] values)
         {
-
+            vgSetParameterfv(handle, paramType, values.Length, values);
         }
 
+        [DllImport(vg, EntryPoint = "vgClear")]
+        extern static void vgClear(int x, int y, int width, int height);
         public void Clear(int x, int y, int width, int height)
         {
-
+            vgClear(x, y, width, height);
         }
 
+        [DllImport(vg, EntryPoint = "vgCreatePath")]
+        extern static uint vgCreatePath(int pathFormat,
+                                PathDatatype datatype,
+                                float scale, float bias,
+                                int segmentCapacityHint,
+                                int coordCapacityHint,
+                                PathCapabilities capabilities);
         public uint CreatePath(
             int pathFormat,
             PathDatatype datatype,
@@ -158,46 +193,68 @@ namespace Amanith
             PathCapabilities capabilities
         )
         {
-            return 0;
+            return vgCreatePath(
+                pathFormat,
+                datatype,
+                scale,
+                bias,
+                segmentCapacityHint,
+                coordCapacityHint,
+                capabilities
+            );
         }
 
+        [DllImport(vg, EntryPoint = "vgDestroyPath")]
+        extern static void vgDestroyPath(uint path);
         public void DestroyPath(uint path)
         {
-
+            vgDestroyPath(path);
         }
 
+        [DllImport(vg, EntryPoint = "vgDrawPath")]
+        extern static void vgDrawPath(uint path, PaintMode paintModes);
         public void DrawPath(uint path, PaintMode paintModes)
         {
-
+            vgDrawPath(path, paintModes);
         }
 
+        [DllImport(vg, EntryPoint = "vgCreatePaint")]
+        extern static uint vgCreatePaint();
         public uint CreatePaint()
         {
-            return 0;
+            return vgCreatePaint();
         }
 
+        [DllImport(vg, EntryPoint = "vgDestroyPaint")]
+        extern static void vgDestroyPaint(uint paint);
         public void DestroyPaint(uint paint)
         {
-
+            vgDestroyPaint(paint);
         }
 
+        [DllImport(vg, EntryPoint = "vgSetPaint")]
+        extern static void vgSetPaint(uint paint, PaintMode paintModes);
         public void SetPaint(uint paint, PaintMode paintModes)
         {
-
+            vgSetPaint(paint, paintModes);
         }
 
         #endregion
 
         #region VGU
 
+        [DllImport(vg, EntryPoint = "vguLine")]
+        extern public static uint vgLine(uint path, float x0, float y0, float x1, float y1);
         public uint Line(uint path, float x0, float y0, float x1, float y1)
         {
-            return 0;
+            return vgLine(path, x0, y0, x1, y1);
         }
 
+        [DllImport(vg, EntryPoint = "vguRoundRect")]
+        extern public static uint vgRoundRect(uint path, float x, float y, float width, float height, float arcWidth, float arcHeight);
         public uint RoundRect(uint path, float x, float y, float width, float height, float arcWidth, float arcHeight)
         {
-            return 0;
+            return vgRoundRect(path, x, y, width, height, arcWidth, arcHeight);
         }
 
         #endregion
