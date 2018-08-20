865160e5cec2090bb33032a6aab06509fd38b244
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-19 22:31:26 -0500

    Reserialize yaml.

--- Controller.cs
+++ Controller.cs
@@ -12,7 +12,7 @@ namespace e_sharp_minor
     public class Controller
     {
         private readonly IOpenVG vg;
-        private AllPrograms programs;
+        private V5.AllPrograms programs;
 
         public Controller(IOpenVG vg) => this.vg = vg;
 
@@ -22,7 +22,15 @@ namespace e_sharp_minor
                 .WithNamingConvention(new UnderscoredNamingConvention())
                 .Build();
 
-            programs = de.Deserialize<AllPrograms>(OpenText("all-programs-v5.yml"));
+            using (var tr = OpenText("all-programs-v5.yml"))
+                programs = de.Deserialize<V5.AllPrograms>(tr);
+
+            var se = new SerializerBuilder()
+                .WithNamingConvention(new UnderscoredNamingConvention())
+                .Build();
+
+            using (var tw = CreateText("all-programs-v6.yml"))
+                se.Serialize(tw, programs);
 
             vg.Setfv(ParamType.VG_CLEAR_COLOR, new float[] { 0.0f, 0.0f, 0.2f, 1.0f });
 
@@ -76,49 +84,52 @@ namespace e_sharp_minor
         }
     }
 
-    public class AllPrograms
+    namespace V5
     {
-        public List<AmpDefault> Amp { get; set; }
-        public List<Program> Programs { get; set; }
-    }
+        public class AllPrograms
+        {
+            public List<AmpDefault> Amp { get; set; }
+            public List<Program> Programs { get; set; }
+        }
 
-    public class AmpDefault
-    {
-        [YamlMember(Alias = "fx_layout")]
-        public List<string> FXLayout { get; set; } // `yaml:"fx_layout"`
-        public int Gain { get; set; }          // `yaml:"gain"`     // amp gain (1-127), 0 means default
-        public int GainLog { get; set; }       // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
-    }
+        public class AmpDefault
+        {
+            [YamlMember(Alias = "fx_layout")]
+            public List<string> FXLayout { get; set; } // `yaml:"fx_layout"`
+            public int Gain { get; set; }          // `yaml:"gain"`     // amp gain (1-127), 0 means default
+            public int GainLog { get; set; }       // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
+        }
 
-    public class Program
-    {
-        public string Name { get; set; } // `yaml:"name"`
-        [YamlMember(Alias = "midi", ApplyNamingConventions = false)]
-        public int MidiProgram { get; set; } // `yaml:"midi"`
-        public int Tempo { get; set; } // `yaml:"tempo"`
-        public int Gain { get; set; } // `yaml:"gain"`     // amp gain (1-127), 0 means default
-        public int GainLog { get; set; } // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
-        public List<AmpDefault> Amp { get; set; } // `yaml:"amp"`
-        [YamlMember(Alias = "scenes", ApplyNamingConventions = false)]
-        public List<SceneDescriptor> SceneDescriptors { get; set; } // `yaml:"scenes"`
-    }
+        public class Program
+        {
+            public string Name { get; set; } // `yaml:"name"`
+            [YamlMember(Alias = "midi", ApplyNamingConventions = false)]
+            public int MidiProgram { get; set; } // `yaml:"midi"`
+            public int Tempo { get; set; } // `yaml:"tempo"`
+            public int Gain { get; set; } // `yaml:"gain"`     // amp gain (1-127), 0 means default
+            public int GainLog { get; set; } // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
+            public List<AmpDefault> Amp { get; set; } // `yaml:"amp"`
+            [YamlMember(Alias = "scenes", ApplyNamingConventions = false)]
+            public List<SceneDescriptor> SceneDescriptors { get; set; } // `yaml:"scenes"`
+        }
 
-    public class SceneDescriptor
-    {
-        public string Name { get; set; }
-        [YamlMember(Alias = "MG", ApplyNamingConventions = false)]
-        public Amp MG { get; set; } // `yaml:"MG"`
-        [YamlMember(Alias = "JD", ApplyNamingConventions = false)]
-        public Amp JD { get; set; } // `yaml:"JD"`
-    }
+        public class SceneDescriptor
+        {
+            public string Name { get; set; }
+            [YamlMember(Alias = "MG", ApplyNamingConventions = false)]
+            public Amp MG { get; set; } // `yaml:"MG"`
+            [YamlMember(Alias = "JD", ApplyNamingConventions = false)]
+            public Amp JD { get; set; } // `yaml:"JD"`
+        }
 
-    public class Amp
-    {
-        public int Gain { get; set; } // `yaml:"gain"`     // amp gain (1-127), 0 means default
-        public int GainLog { get; set; } // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
-        public string Channel { get; set; } // `yaml:"channel"`  // "clean" or "dirty"
-        public double Level { get; set; } // `yaml:"level"`    // pre-delay volume in dB (-inf to +6dB)
-        [YamlMember(Alias = "fx", ApplyNamingConventions = false)]
-        public List<string> FX { get; set; } // `yaml:"fx,flow"`  // any combo of "delay", "pitch", or "chorus"
+        public class Amp
+        {
+            public int Gain { get; set; } // `yaml:"gain"`     // amp gain (1-127), 0 means default
+            public int GainLog { get; set; } // `yaml:"gain_log"` // amp gain (1-127) in log scale, 0 means default
+            public string Channel { get; set; } // `yaml:"channel"`  // "clean" or "dirty"
+            public double Level { get; set; } // `yaml:"level"`    // pre-delay volume in dB (-inf to +6dB)
+            [YamlMember(Alias = "fx", ApplyNamingConventions = false)]
+            public List<string> FX { get; set; } // `yaml:"fx,flow"`  // any combo of "delay", "pitch", or "chorus"
+        }
     }
 }
