67ac79d8b5325e6462bd8c73c14d96f8a3da377c
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-22 12:08:53 -0500

    Dummy IMIDI implementation to write to Console.

---
+++ MidiConsoleOut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace e_sharp_minor
+{
+	public class MidiConsoleOut : IMIDI
+    {
+        public MidiConsoleOut()
+        {
+        }
+
+        public void SetController(int channel, int controller, int value)
+        {
+            Console.WriteLine("MIDI: {0:X2} {1:X2} {2:X2}", 0xB0 | (channel & 15), controller, value);
+        }
+
+        public void SetProgram(int channel, int program)
+        {
+            Console.WriteLine("MIDI: {0:X2} {1:X2}", 0xC0 | (channel & 15), program);
+        }
+    }
+}
