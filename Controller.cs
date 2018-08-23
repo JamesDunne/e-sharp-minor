16d9a6838a9145582c9b22a0a0913b9b237397f1
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-08-23 12:21:48 -0500

    Working CC inheritance and sending commands.

--- Controller.cs
+++ Controller.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using e_sharp_minor.V6;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using static System.IO.File;
+using static System.Math;
 
 namespace e_sharp_minor
 {
@@ -112,8 +114,19 @@ namespace e_sharp_minor
             ActivateSong(songsSorted[0]);
         }
 
+        int DBtoMIDI(double db)
+        {
+            db = db - 6.0;
+            double p = Pow(10.0, (db / 20.0));
+            double plog = Log10(p * 14.5 + 1.0) / Log10(15.5);
+            plog *= 127.0;
+            return (int)(Round(plog));
+        }
+
         public void ActivateSong(Song newSong)
         {
+            Console.WriteLine("Activate song '{0}'", newSong.Name);
+
             int scene = 0;
 
             if (currentSong != null)
@@ -121,13 +134,81 @@ namespace e_sharp_minor
                 // TODO
             }
 
+            Console.WriteLine("Change MIDI program {0}", newSong.MidiProgram.ProgramNumber);
             midi.SetProgram(channel, newSong.MidiProgram.ProgramNumber);
 
-            // TODO
-            //midi.SetController(channel, );
-            //newSong.MidiProgram.Amps[0];
-            //newSong.Amps[0];
-            //newSong.SceneDescriptors[scene].Amps[0].Tone;
+            for (int i = 0; i < newSong.MidiProgram.Amps.Count; i++)
+            {
+                SceneDescriptor sceneDescriptor = newSong.SceneDescriptors[scene];
+                ToneSelection toneSelection = sceneDescriptor.Amps[i];
+                ToneDefinition toneDefinition = toneSelection.ToneDefinition;
+                AmpDefinition ampDefinition = toneDefinition.AmpDefinition;
+
+                // Figure out the song-specific override of tone:
+                ToneOverride toneOverride = null;
+                if (newSong.Amps != null)
+                {
+                    toneOverride = newSong.Amps[i].Tones[toneSelection.Tone];
+                }
+                else
+                {
+                    toneOverride = new ToneOverride
+                    {
+                        Blocks = new Dictionary<string, FXBlockOverride>()
+                    };
+                }
+
+                // Set all the controller values for the selected tone:
+                foreach (var pair in toneDefinition.Blocks)
+                {
+                    FXBlock blockDefault = pair.Value;
+                    string blockName = pair.Key;
+                    FXBlockDefinition blockDefinition = ampDefinition.Blocks[blockName];
+
+                    int enabledCC = blockDefinition.EnabledSwitchCC;
+                    int? xySwitchCC = blockDefinition.XYSwitchCC;
+
+                    FXBlockOverride songBlockOverride = null;
+                    if (toneOverride != null)
+                    {
+                        songBlockOverride = toneOverride.Blocks.GetValueOrDefault(blockName);
+                    }
+                    if (songBlockOverride == null)
+                    {
+                        songBlockOverride = new FXBlockOverride();
+                    }
+
+                    // Follow inheritance chain to determine enabled and X/Y switch values:
+                    Dictionary<string, FXBlockOverride> toneSelectionBlocks = toneSelection.Blocks ?? new Dictionary<string, FXBlockOverride>();
+                    var sceneBlockOverride = toneSelectionBlocks.GetValueOrDefault(blockName) ?? new FXBlockOverride();
+
+                    var enabled = sceneBlockOverride.On ?? songBlockOverride.On ?? blockDefault.On;
+                    var xy = sceneBlockOverride.XY ?? songBlockOverride.XY ?? blockDefault.XY;
+
+                    if (enabled.HasValue)
+                    {
+                        Console.WriteLine("Amp[{0}]: {1} = {2}", i+1, blockName, enabled.Value ? "on" : "off");
+                        midi.SetController(channel, enabledCC, enabled.Value ? 0x7F : 0x00);
+                    }
+                    if (xy.HasValue && xySwitchCC.HasValue)
+                    {
+                        Console.WriteLine("Amp[{0}]: {1} to {2}", i + 1, blockName, enabled.Value ? "X" : "Y");
+                        midi.SetController(channel, xySwitchCC.Value, xy.Value == XYSwitch.X ? 0x7F : 0x00);
+                    }
+                }
+
+                // Set the gain and volume:
+                var gain = toneSelection.Gain ?? toneOverride.Gain ?? toneDefinition.Gain;
+                var volume = toneSelection.Volume ?? toneOverride.Volume ?? toneDefinition.Volume;
+
+                // Convert volume to MIDI value:
+                var volumeMIDI = DBtoMIDI(volume);
+
+                Console.WriteLine("Amp[{0}]: gain   (CC {1:X2}h) to {2:X2}h", i + 1, toneDefinition.AmpDefinition.GainControllerCC, gain);
+                midi.SetController(channel, toneDefinition.AmpDefinition.GainControllerCC, gain);
+                Console.WriteLine("Amp[{0}]: volume (CC {1:X2}h) to {2:X2}h ({3} dB)", i + 1, toneDefinition.AmpDefinition.VolumeControllerCC, volumeMIDI, volume);
+                midi.SetController(channel, toneDefinition.AmpDefinition.VolumeControllerCC, volumeMIDI);
+            }
 
             this.currentSong = newSong;
         }
