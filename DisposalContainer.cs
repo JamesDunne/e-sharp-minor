bf949a2b8d843e7bbe4525aadaf8ba77b3679183
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-09-01 16:56:40 -0500

    Extend DisposalContainer

--- DisposalContainer.cs
+++ DisposalContainer.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace e_sharp_minor
 {
     public class DisposalContainer : IDisposable
     {
-        private readonly IDisposable[] objects;
+        private readonly List<IDisposable> objects;
 
-        public DisposalContainer(params IDisposable[] objects) => this.objects = objects;
+        public DisposalContainer(params IDisposable[] objects) => this.objects = new List<IDisposable>(objects);
+
+        public T Add<T>(T disposable) where T : IDisposable
+        {
+            objects.Add(disposable);
+            return disposable;
+        }
 
         public void Dispose()
         {
