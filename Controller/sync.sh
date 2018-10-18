d2d0dc07a7dc4385f8d2b365de81198a79313cea
Jim Dunne <james.jdunne@gmail.com>
2018-10-17 21:42:30 -0500

    Slightly more efficient rsync, maybe.

--- Controller/sync.sh
+++ Controller/sync.sh
@@ -2,4 +2,5 @@
 if ! dotnet publish --configuration pi-release --runtime linux-arm; then
 	exit 1
 fi
-rsync -Pr bin/pi-release/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
+#rsync -Pr bin/pi-release/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
+rsync -a -H -x -v -r --delete --progress -e "ssh -T -o Compression=no -x" bin/pi-release/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
