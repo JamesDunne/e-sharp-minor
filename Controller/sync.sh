0a81026cbf328b83f0b324aa825b996f0b8f6190
Jim Dunne <james.jdunne@gmail.com>
2018-10-23 16:33:35 -0500

    Update sync.sh to select configuration.

--- Controller/sync.sh
+++ Controller/sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if ! dotnet publish --configuration pi-release --runtime linux-arm; then
+#cfg=pi-release
+cfg=pi-debug
+if ! dotnet publish --configuration $cfg --runtime linux-arm; then
 	exit 1
 fi
-#rsync -Pr bin/pi-release/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
-rsync -a -H -x -v -r --delete --progress -e "ssh -T -o Compression=no -x" bin/pi-release/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
+rsync -a -H -x -v -r --delete --progress -e "ssh -T -o Compression=no -x" bin/$cfg/netcoreapp2.1/linux-arm/publish/ pi:e-sharp-minor/
